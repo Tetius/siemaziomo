b7b5b5708bec4978db6a0662f1bc4429959c207a
mwazelinski <mwazelinski@gmail.com>
2016-04-15 20:02:52 +0200

    instagram api client

---
+++ hack4wroAPI/src/hack4wroAPI/Services/InstagramService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Globalization;
+using Newtonsoft.Json.Linq;
+
+namespace hack4wroAPI.Services
+{
+    public class InstagramService : IInstagramService
+    {
+        string clientId = "3422eb9eb676411a9ba67d9535195810";
+        string clientSecret = "5657cfff43d14ff6b20f4804c771cfaa";
+
+        Uri apiUrl = new Uri("https://api.instagram.com/v1/");
+
+        public InstagramService()
+        {
+
+        }
+
+        public async Task<dynamic> GetLocations(double lat, double lng, double distance, string accessToken)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = apiUrl;
+                client.DefaultRequestHeaders.Accept.Clear();
+                var path = string.Format($"locations/search?distance={distance.ToString(CultureInfo.InvariantCulture)}&lat={lat.ToString(CultureInfo.InvariantCulture)}&lng={lng.ToString(CultureInfo.InvariantCulture)}&access_token={accessToken}");
+                var response = await client.GetAsync(path);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseJson = await response.Content.ReadAsStringAsync();
+                    return await Task.Run(() => JObject.Parse(responseJson));
+                }
+                throw new InvalidOperationException();
+            }
+        }
+        
+    }
+}
